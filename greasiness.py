78fc826a648f215a6ee521dec8ab12dc469acf61
Aryan Sethi <46836282+AryanSethi@users.noreply.github.com>
2020-06-10 18:24:08 +0530

    Add files via upload

--- greasiness.py
+++ greasiness.py
@@ -8,8 +8,8 @@ blurred = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred=cv2.resize(blurred,(500,500))
 cv2.imshow('blurred gray',blurred)
 
-start=100
-increament=30
+start=110
+increament=20
 
 for _ in range(1):
     image_name= str(start)+"-"+str(start+increament)
@@ -17,23 +17,20 @@ for _ in range(1):
     _,thresh= cv2.threshold(thresh, start, 255, cv2.THRESH_TOZERO)
     thresh= cv2.resize(thresh, (500, 500))
     _,thresh = cv2.threshold(thresh,1, 255, cv2.THRESH_BINARY)
-    cv2.imshow(image_name, thresh)
+    #cv2.imshow(image_name, thresh)
     start=start+increament
+
+
 
 
 kernel = np.ones((2,2),np.uint8)
 dilation = cv2.dilate(thresh,kernel,iterations = 1)
 final= cv2.bitwise_and(blurred,dilation)
-cv2.imshow('final',final)
+cv2.imshow("final1",final)
 
-# median=cv2.medianBlur(thresh,3)
-# cv2.imshow('median original',median)
-# gaussian=cv2.GaussianBlur(thresh,(5,5),100)
-# cv2.imshow('gaussian',gaussian)
-# _,final=cv2.threshold(gaussian,15,255,cv2.THRESH_BINARY)
+_,final = cv2.threshold(final,1, 150, cv2.THRESH_BINARY)
+cv2.imshow('final2',final)
 
-#final=cv2.bitwise_and(final,blurred)
-# cv2.imshow('final',final)
 
 
 cv2.waitKey(0)
